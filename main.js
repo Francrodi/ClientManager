2f5234e8b96fa0b588039d91fe69424bbff04bc6
Francisco Crocamo <frcrocamo@gmail.com>
2020-02-29 19:57:34 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,20 @@
+const {app, BrowserWindow} = require('electron');
+require('electron-reload')(__dirname)
+function createWindow() {
+
+
+
+    let win = new BrowserWindow({
+        width: 600,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+
+    })
+
+win.loadFile('index.html')
+
+}
+
+app.whenReady().then(createWindow)
