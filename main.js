f184eb705db2ab292d4d2a2bf2ed2ebb14f9c924
Francisco Crocamo <frcrocamo@gmail.com>
2020-03-01 17:24:05 -0300

    Nuevo layout

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 const {app, BrowserWindow} = require('electron');
 require('electron-reload')(__dirname)
 function createWindow() {
-
-
-
     let win = new BrowserWindow({
         width: 600,
         height: 600,
         webPreferences: {
             nodeIntegration: true
         }
-
     })
 
 win.loadFile('index.html')
