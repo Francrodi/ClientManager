07457d033d32a7a905a04155c71e3afaf94434fb
Francisco Crocamo <frcrocamo@gmail.com>
2020-05-05 00:39:50 -0300

    sidebar css

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
-const {app, BrowserWindow, BrowserView} = require('electron');
-require('electron-reload')(__dirname)
-
+const { app, BrowserWindow, BrowserView } = require("electron");
+require("electron-reload")(__dirname);
 
 function createWindow() {
-
     let mainWindow = new BrowserWindow({
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
         },
         show: false,
-        height: 600,
-        width: 800
-
-        
+        height: 800,
+        width: 800,
     });
     let winVisor = mainWindow.getBounds();
     console.log(winVisor.height);
     let sidebar = new BrowserView();
     mainWindow.addBrowserView(sidebar);
-    sidebar.setBounds({x: 0, y: 0, width: winVisor.width*0.30, height: winVisor.height});
-    sidebar.webContents.loadFile('sidebar.html');
-    sidebar.setAutoResize({width: true, height: true, horizontal: true, vertical: true});
+    sidebar.setBounds({
+        x: 0,
+        y: 0,
+        width: winVisor.width * 0.3,
+        height: 800,
+    });
+    sidebar.webContents.loadFile("./src/sidebar.html");
+    sidebar.setAutoResize({
+        width: true,
+        height: true,
+        horizontal: true,
+        vertical: true,
+    });
 
     let content = new BrowserView();
     mainWindow.addBrowserView(content);
-    content.setBounds({x: winVisor.width*0.30, y: 0, width: winVisor.width*0.70, height: winVisor.height});
-    content.webContents.loadFile('content.html');
-    content.setAutoResize({width: true, height: true, horizontal: true, vertical: true});
+    content.setBounds({
+        x: winVisor.width * 0.3,
+        y: 0,
+        width: winVisor.width * 0.7,
+        height: 800,
+    });
+    content.webContents.loadFile("content.html");
+    content.setAutoResize({
+        width: true,
+        height: true,
+        horizontal: true,
+        vertical: true,
+    });
     mainWindow.show();
 }
-
 
 app.whenReady().then(createWindow);
